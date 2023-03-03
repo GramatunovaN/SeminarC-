7e48a632b9d9ebf8171fe44d000c4b830e5da162
GramatunovaN <gramatunova.nata@mail.ru>
2023-03-04 00:03:54 +0700

    removesecondnumber

---
+++ Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число - {number}");
+int removesecond = RemoveSecond(number);
+Console.WriteLine($"Результат = {removesecond}");
+
+int RemoveSecond (int number) 
+{
+int first = number/100;
+int last = number%10;
+int result = first * 10 + last;
+return result;
+}
