684fdb5e1adeb8738e34a9c28e1ea5a3f62eaf0c
GramatunovaN <gramatunova.nata@mail.ru>
2023-03-03 23:45:55 +0700

    secondnumber

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трехзначное число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB=numberA/10;
+if (numberA>999)
+{ 
+    Console.WriteLine($"{numberA} - не трехзначное число");
+} 
+else if(numberA<100)
+{ 
+    Console.WriteLine($"{numberA} - не трехзначное число");
+} 
+else 
+{   
+     Console.WriteLine($"{numberB%10} - вторая цифра");
+}
