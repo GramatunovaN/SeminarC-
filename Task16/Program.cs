a659f5fff0a1f9b3a6b99e6404a7542ba77da68a
GramatunovaN <gramatunova.nata@mail.ru>
2023-11-12 21:50:58 +0500

    16+

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -9,5 +9,6 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 int numberB = Convert.ToInt32(Console.ReadLine());
 
 if (numberB==numberA*numberA) Console.Write($"Число {numberB} является квадратом числа {numberA} ");
-else   Console.Write($"Число {numberB} не является квадратом числа {numberA} ");
+else if (numberA==numberB*numberB) Console.Write($"Число {numberA} является квадратом числа {numberB} ");
+else   Console.Write("Числа не являются квадратами друг друга");
 
